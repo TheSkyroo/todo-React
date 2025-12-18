3d0e691974de88358ea1984d6bb8e60cd318a2b3
TheSkyroo <ishaant69@gmail.com>
2025-12-18 21:55:55 +0530

    done

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,51 +1,56 @@
 import React, { useState } from "react";
 
-function Todolist(){
-    const [todo,setTodo]=useState("");
-    const [listTodo,setListData]=useState([])
-    function addTodo(){
-       
-        setListData((listTodo)=>{
-            const updatedList =[...listTodo,todo]
-            setTodo('');
-            return updatedList
-        })
-        
+function Todolist() {
+  const [todo, setTodo] = useState("");
+  const [listTodo, setListData] = useState([]);
+  function addTodo() {
+    setListData((listTodo) => {
+      const updatedList = [...listTodo, todo];
+      setTodo("");
+      return updatedList;
+    });
+  }
+  function doneTodo(i) {
+    const updatedListData = listTodo.filter((elem, id) => {
+      return i != id;
+    });
+    setListData(updatedListData);
+  }
+  function removeAll() {
+    setListData("");
+  }
+  return (
+    <>
+      <div className="container">
+        <div className="header">Todo List</div>
+        <input
+          type="text"
+          placeholder="Add Todos"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+        <p className="listHeading">Here are your Tods🔥</p>
+        {listTodo != [] &&
+          listTodo.map((todo, i) => {
+            return (
+              <>
+                <p key={i}>
+                  <div className="listTodos">{todo}</div>
+                  <div>
+                    <button onClick={() => doneTodo(i)}>Done</button>
+                  </div>
+                </p>
+              </>
+            );
+          })}
 
-    }
-    function doneTodo(i){
-        const updatedListData = listTodo.filter((elem,id)=>{
-            return i!=id;
-        })
-        setListData(updatedListData)
-    }
-    function removeAll(){
-         setListData('');
-
-    }
-    return(
-        <>
-            <div className="container">
-                <div className="header">Todo List</div>
-                <input type="text" placeholder="Add Todos" value={todo} onChange={(e)=>setTodo(e.target.value)}/>
-                 <button onClick={addTodo}>Add Todo</button>
-                 <p className="listHeading">Here are your Tods🔥</p>
-                 {listTodo!=[] && listTodo.map((todo,i)=>{
-                    return(
-                        <>
-                        <p key={i}>
-                            <div className="listTodos">{todo}</div>
-                           <div><button onClick={()=>doneTodo(i)}>Done</button></div> 
-                        </p>
-                        </>
-                    )
-                 })}
-
-                 {listTodo.length>=1 && <button onClick={removeAll}> Remove All</button>}
-                 
-            </div>
-        </>
-    )
+        {listTodo.length >= 1 && (
+          <button onClick={removeAll}> Remove All</button>
+        )}
+      </div>
+    </>
+  );
 }
 
-export default Todolist
+export default Todolist;
